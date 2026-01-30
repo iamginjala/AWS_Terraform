d2c818cd99de1879ed5c454b187cf10598d96223
Harsha <harshaginjaladec23@gmail.com>
2026-01-30 08:31:39 -0500

    ec2 instance

---
+++ EC2-INSTANCE/main.tf
@@ -0,0 +1,44 @@
+
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "${var.environment}-test-bucket-2308"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+    filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] 
+
+}
+
+resource "aws_instance" "test_ec2" {
+  count = var.instance_count
+  ami =  data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  monitoring = var.monitoring_enabled
+  associate_public_ip_address = var.associate_public_ip
+  
+}
+
+resource "aws_vpc" "test_vpc" {
+  cidr_block = var.cidr_blocks[0]
+}
+resource "aws_subnet" "public" {
+  vpc_id = aws_vpc.test_vpc.id
+  cidr_block = var.cidr_blocks[1]
+  map_public_ip_on_launch = var.associate_public_ip
+
+  
+}
+resource "aws_subnet" "private" {
+  vpc_id = aws_vpc.test_vpc.id
+  cidr_block = var.cidr_blocks[2]
+}
