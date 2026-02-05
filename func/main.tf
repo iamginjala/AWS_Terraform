7570fe18ea4041a6965c47e49d23009aafaebe64
Harsha <harshaginjaladec23@gmail.com>
2026-02-05 09:24:27 -0500

    functions

--- func/main.tf
+++ func/main.tf
@@ -1,108 +1,108 @@
-# locals {
-#   formatted_project_name = replace(lower(var.project_name), " ", "-")
-#   replace_bucket_name = replace(trim(lower(var.bucket_name)," !"), " ", "")
-#   port_list = split(",",var.allowed_ports)
-#   ports = [ for port in local.port_list :{
-#     name = "port - ${port}"
-#     port = tonumber(port)
-#     description = "Allow traffic on port ${port}"
-#   }]
-#   formatted_ports = join("-", [for port in local.port_list : "port-${port}"])
-#   instance_sizes = lookup(var.instance_sizes,var.environment,"t2.micro")
-# }
+locals {
+  formatted_project_name = replace(lower(var.project_name), " ", "-")
+  replace_bucket_name = replace(trim(lower(var.bucket_name)," !"), " ", "")
+  port_list = split(",",var.allowed_ports)
+  ports = [ for port in local.port_list :{
+    name = "port - ${port}"
+    port = tonumber(port)
+    description = "Allow traffic on port ${port}"
+  }]
+  formatted_ports = join("-", [for port in local.port_list : "port-${port}"])
+  instance_sizes = lookup(var.instance_sizes,var.environment,"t2.micro")
+}
 
 
 
-# # output "test-split" {
-# #     value = local.ports
+output "test-split" {
+    value = local.ports
   
-# # }
-# # output "test_bucket_name" { 
-# #     value = local.replace_bucket_name
+}
+output "test_bucket_name" { 
+    value = local.replace_bucket_name
   
-# # }
+}
 
-# resource "aws_s3_bucket" "test-bucket" {
-#     bucket = local.replace_bucket_name
-#     tags = merge(var.default_tags, var.environment_tags)
-# }
+resource "aws_s3_bucket" "test-bucket" {
+    bucket = local.replace_bucket_name
+    tags = merge(var.default_tags, var.environment_tags)
+}
 
-# resource "aws_vpc" "sg_vpc" {
-#   cidr_block = "10.0.0.0/16"
-#   tags = {
-#     name = "${local.formatted_project_name}-vpc"
-#     assignment = 4
-#   }
+resource "aws_vpc" "sg_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    name = "${local.formatted_project_name}-vpc"
+    assignment = 4
+  }
   
-# }
+}
 
-# resource "aws_security_group" "test_sg" {
-#     name = "${local.formatted_project_name}-sg"
-#     vpc_id = aws_vpc.sg_vpc.id
+resource "aws_security_group" "test_sg" {
+    name = "${local.formatted_project_name}-sg"
+    vpc_id = aws_vpc.sg_vpc.id
 
-#     dynamic "ingress" {
-#         for_each = local.ports
-#         content {
-#             from_port = ingress.value.port
-#             to_port = ingress.value.port
-#             protocol = "tcp"
-#             cidr_blocks = ["0.0.0.0/0"]
-#         }
-#     }
-#     egress {
-#         from_port = 0
-#         to_port = 0
-#         protocol = "-1"
-#         cidr_blocks = ["0.0.0.0/0"]
-#     }
+    dynamic "ingress" {
+        for_each = local.ports
+        content {
+            from_port = ingress.value.port
+            to_port = ingress.value.port
+            protocol = "tcp"
+            cidr_blocks = ["0.0.0.0/0"]
+        }
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
   
-# }
-# data "aws_ami" "amazon_linux" {
-#   most_recent = true
-#   owners      = ["amazon"]
+}
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
 
-#   filter {
-#     name   = "name"
-#     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-#   }
-# }
-# resource "aws_instance" "app_server" {
-#     ami = data.aws_ami.amazon_linux.id
-#     instance_type = local.instance_sizes
-#     security_groups = [aws_security_group.test_sg.name]
-#     tags = {
-#         name = "app-server-${var.environment}"
-#         environment = var.environment
-#         instance_type = local.instance_sizes
-#     }
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+resource "aws_instance" "app_server" {
+    ami = data.aws_ami.amazon_linux.id
+    instance_type = local.instance_sizes
+    security_groups = [aws_security_group.test_sg.name]
+    tags = {
+        name = "app-server-${var.environment}"
+        environment = var.environment
+        instance_type = local.instance_sizes
+    }
 
-# }
-# locals {
-#     backup_config ={
-#         name = var.backup_name
-#         credential = var.credential
-#         enabled = true
-#     }
-# }
+}
+locals {
+    backup_config ={
+        name = var.backup_name
+        credential = var.credential
+        enabled = true
+    }
+}
 
-# output "backup_sensitive" {
-#     value = local.backup_config
+output "backup_sensitive" {
+    value = local.backup_config
   
-# }
+}
 
-# data "aws_ami" "valaidated_ami" {
-#     most_recent = true
-#     owners = ["amazon"]
+data "aws_ami" "valaidated_ami" {
+    most_recent = true
+    owners = ["amazon"]
 
-#     filter {
-#         name = "name"
-#         values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-#     }
-# }
-# resource "aws_instance" "validated_instance" {
-#     ami = data.aws_ami.valaidated_ami.id
-#     instance_type = var.instance_type
-# }
+    filter {
+        name = "name"
+        values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    }
+}
+resource "aws_instance" "validated_instance" {
+    ami = data.aws_ami.valaidated_ami.id
+    instance_type = var.instance_type
+}
 
 locals {
   # Check if files exist
