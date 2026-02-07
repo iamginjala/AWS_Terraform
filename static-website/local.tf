0e0aa9fbc2cfac51955ccf40aa910d64c8c6da52
Harsha <harshaginjaladec23@gmail.com>
2026-02-07 13:09:50 -0500

    static website

---
+++ static-website/local.tf
@@ -0,0 +1,10 @@
+locals {
+  website_files = ["index.html","error.html"]
+  content_types = {
+    "html" = "text/html"
+    "css"  = "text/css"
+    "js"   = "application/javascript"
+    "png"  = "image/png"
+    "jpg"  = "image/jpeg"
+  }
+}
