f5269e6b1cde2a89149b3387a7f295244b828999
Harsha <harshaginjaladec23@gmail.com>
2026-02-08 09:59:21 -0500

    vpc-peering

---
+++ vpc-peering/peering.tf
@@ -0,0 +1,21 @@
+resource "aws_vpc_peering_connection" "test_peer" {
+    peer_vpc_id = aws_vpc.vpc_accepter.id
+    vpc_id = aws_vpc.vpc_requester.id
+    peer_region = "us-west-2"
+    auto_accept = false
+    tags = {
+      Side = "Requester"
+   }
+}
+
+
+resource "aws_vpc_peering_connection_accepter" "test_accept" {
+  vpc_peering_connection_id = aws_vpc_peering_connection.test_peer.id
+  auto_accept = true
+  provider = aws.peer
+
+  tags = {
+    Side = "Accepter"
+  }
+
+}
