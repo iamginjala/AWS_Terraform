136eff700236fb103edbe6efaea7804297effff5
Harsha <harshaginjaladec23@gmail.com>
2026-02-10 08:28:53 -0500

    vpc-peering

--- vpc-peering/variables.tf
+++ vpc-peering/variables.tf
@@ -10,3 +10,15 @@ variable "cidr_block_accepter" {
     default = "11.0.0.0/16"
     description = "cidr block range for vpc accepter"
 }
+
+variable "requester_subnet" {
+  type = string
+  default = "10.0.1.0/24"
+  description = "cidr block range for requester subnet"
+}
+
+variable "accepter_subnet" {
+  type = string
+  default = "11.0.1.0/24"
+  description = "cidr block range for accepter subnet"
+}
