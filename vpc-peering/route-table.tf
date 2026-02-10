11423cfcbb0ac254377d6282a8aabb4a1e81bbab
Harsha <harshaginjaladec23@gmail.com>
2026-02-10 08:28:23 -0500

    vpc-peering

--- vpc-peering/route-table.tf
+++ vpc-peering/route-table.tf
@@ -1,15 +1,42 @@
 
-resource "aws_route" "requester_route" {
-    route_table_id = aws_vpc.vpc_requester.main_route_table_id
-    destination_cidr_block = var.cidr_block_accepter
-    vpc_peering_connection_id = aws_vpc_peering_connection.test_peer.id
+resource "aws_route_table" "request_route" {
+    vpc_id = aws_vpc.vpc_requester.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.request_igw.id
+    }
+    route {
+        cidr_block = var.cidr_block_accepter
+        vpc_peering_connection_id = aws_vpc_peering_connection.test_peer.id
+    }
+  
+}
+resource "aws_route_table" "accept_route" {
+    vpc_id = aws_vpc.vpc_accepter.id
+    provider = aws.peer
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.accept_igw.id
+    }
+    route {
+        cidr_block = var.cidr_block_requester
+        vpc_peering_connection_id = aws_vpc_peering_connection.test_peer.id
+    }
+
+
+}
+
+resource "aws_route_table_association" "accept_rta" {
+    provider = aws.peer
+    subnet_id = aws_subnet.accept_subnet.id
+    route_table_id = aws_route_table.accept_route.id
   
 }
 
-resource "aws_route" "acceptor_route" {
-    route_table_id = aws_vpc.vpc_accepter.main_route_table_id
-    destination_cidr_block = var.cidr_block_requester
-    vpc_peering_connection_id = aws_vpc_peering_connection.test_peer.id
-    provider = aws.peer
-  
+resource "aws_route_table_association" "request_rta" {
+  subnet_id = aws_subnet.request_subnet.id
+  route_table_id = aws_route_table.request_route.id
 }
+
