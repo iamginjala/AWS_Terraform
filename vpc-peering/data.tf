f2edfb14d7dddeca5e574081de1deb2b9b769388
Harsha <harshaginjaladec23@gmail.com>
2026-02-10 08:28:41 -0500

    vpc-peering

---
+++ vpc-peering/data.tf
@@ -0,0 +1,48 @@
+data "aws_availability_zones" "accepter" {
+  state    = "available"
+  provider = aws.peer
+}
+
+data "aws_availability_zones" "requester" {
+  state    = "available"
+  
+}
+data "aws_ami" "accepter_ami" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical (Ubuntu)
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+data "aws_ami" "requester_ami" {
+  provider    = aws.primary
+  most_recent = true
+  owners      = ["099720109477"] # Canonical (Ubuntu)
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
