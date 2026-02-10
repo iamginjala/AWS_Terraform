1eba8c90c0a7c43f225e788ce67ef8c11b11bf7b
Harsha <harshaginjaladec23@gmail.com>
2026-02-10 08:29:26 -0500

    vpc-peering

--- vpc-peering/vpc-requester.tf
+++ vpc-peering/vpc-requester.tf
@@ -1,5 +1,16 @@
 resource "aws_vpc" "vpc_requester" {
     cidr_block = var.cidr_block_requester
-    
+      
+}
+
+resource "aws_subnet" "request_subnet" {
+    vpc_id = aws_vpc.vpc_requester.id
+    cidr_block = var.requester_subnet
+    map_public_ip_on_launch = true
+    availability_zone = data.aws_availability_zones.requester.names[0]
   
+}
+resource "aws_internet_gateway" "request_igw" {
+  vpc_id = aws_vpc.vpc_requester.id
+
 }
