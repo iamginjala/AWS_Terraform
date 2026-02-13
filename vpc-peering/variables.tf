60ea553e6e3f9f52523768026aa3ec739c8fb7a1
Harsha <harshaginjaladec23@gmail.com>
2026-02-13 15:48:11 -0500

    transistive peering

--- vpc-peering/variables.tf
+++ vpc-peering/variables.tf
@@ -11,6 +11,12 @@ variable "cidr_block_accepter" {
     description = "cidr block range for vpc accepter"
 }
 
+variable "cidr_block_transistive" {
+    type = string
+    default = "12.0.0.0/16"
+    description = "cidr block range for vpc accepter transistive"
+}
+
 variable "requester_subnet" {
   type = string
   default = "10.0.1.0/24"
@@ -22,3 +28,21 @@ variable "accepter_subnet" {
   default = "11.0.1.0/24"
   description = "cidr block range for accepter subnet"
 }
+variable "transistive_subnet" {
+  type = string
+  default = "12.0.1.0/24"
+  description = "cidr block range for transistive subnet"
+}
+
+variable "primary_key_name" {
+  description = "Name of the SSH key pair for request VPC instance (us-east-1)"
+  type        = string
+  default     = "vpc-peering-demo"
+}
+
+variable "secondary_key_name" {
+  description = "Name of the SSH key pair for accept VPC instance (us-west-2)"
+  type        = string
+  default     = "vpc-peering-demo-west"
+}
+
