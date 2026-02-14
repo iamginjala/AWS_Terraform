db70df202b497a9e67e0f4694ef69993836d08cd
Harsha <harshaginjaladec23@gmail.com>
2026-02-14 15:17:33 -0500

    Blue-Green deployment

---
+++ Blue-GreenDeployment/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers{
+  aws = {
+      source = "hashicorp/aws"
+      version = "6.31.0"
+  }
+  archive = {
+      source = "hashicorp/archive"
+      version = "2.7.1"
+  }
+}
+}
+
+
+provider "aws" {
+  region = var.region
+}
