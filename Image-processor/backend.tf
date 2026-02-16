df362ce427bde3e1b516eb91163d4cfcf97e49cc
Harsha <harshaginjaladec23@gmail.com>
2026-02-16 08:55:51 -0500

    Image Processor

---
+++ Image-processor/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket = "deom-for-tfstate-files-0128"
+    encrypt = true
+    key = "dev/image-processor/terraform.tfstate"
+    region = "us-east-2"
+    use_lockfile = true
+    
+  }
+}
