696b88e33176d5dbe5e26b5822cb2080b347a004
Harsha <harshaginjaladec23@gmail.com>
2026-02-18 12:16:31 -0500

    provisioners with aws

---
+++ provisioners/variables.tf
@@ -0,0 +1,15 @@
+variable "key_name" {
+  description = "Name of an existing EC2 key pair (must already exist in the chosen region)"
+  type        = string
+}
+
+variable "private_key_path" {
+  description = "Path to the private key file for SSH (used by remote provisioners)"
+  type        = string
+}
+
+variable "ssh_user" {
+  description = "SSH username for the AMI (default for Ubuntu is 'ubuntu')"
+  type        = string
+  default     = "ubuntu"
+}
