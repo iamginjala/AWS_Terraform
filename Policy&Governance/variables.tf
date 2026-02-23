f944d9cae4353db580aa806ee0fd3c9b0671b788
Harsha <harshaginjaladec23@gmail.com>
2026-02-23 11:08:03 -0500

    feat: implemented AWS policy & Governance using Terraform

---
+++ Policy&Governance/variables.tf
@@ -0,0 +1,10 @@
+variable "region" {
+  type = string
+  default = "us-east-2"
+  description = "default region for the aws account"
+}
+variable "project_name" {
+  type = string
+  default = "policy-governance"
+  description = "project name for the aws resources"
+}
